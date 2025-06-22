2d86a591d4f6abe0ee045cc7c030a092e248b47d
MdAfsarHossain <afsar291722@gmail.com>
2025-06-22 16:51:24 +0600

    App ts file updated

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { booksRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 const app: Application = express();
@@ -12,6 +12,22 @@ app.use("/api/borrow", borrowRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library Management System!!");
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log("Error: ", error);
+    res.status(400).json({
+      message: "Something wrong from global error",
+      error,
+    });
+  }
 });
 
 export default app;
