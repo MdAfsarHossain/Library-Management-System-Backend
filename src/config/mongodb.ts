4596325cbfd55088a5236c730e8223b1500ffce4
MdAfsarHossain <afsar291722@gmail.com>
2025-06-22 10:17:52 +0600

    mongodb file updated

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 // MONGO_DB
-const uri = `mongodb+srv://libraryManagementSystem:FUtusWaWDXxYdY3S@cluster0.b6ov8m0.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://<db_name>:<db_pass>@cluster0.b6ov8m0.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
